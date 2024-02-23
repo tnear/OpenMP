bdd289511dd7a83c1f59089f2f598fc734b6dfb8
tnear <11488488+tnear@users.noreply.github.com>
2024-02-23 11:25:47 -0600

    ParallelFor (matrix multiply)

--- ParallelFor.cpp
+++ ParallelFor.cpp
@@ -51,10 +51,51 @@ void testLoopIndependence()
     assert(result == 1'006'000);
 }
 
+void matrixMultiply(const vector<vector<int>> &A, const vector<vector<int>> &B,
+                    vector<vector<int>> &C)
+{
+    int N = A.size();
+    #pragma omp parallel for
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            C[i][j] = 0;
+            for (int k = 0; k < N; ++k)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void testMatrixMultiply()
+{
+    const int N = 3;
+    vector<vector<int>> A = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
+
+    vector<vector<int>> B = {
+        {9, 8, 7},
+        {6, 5, 4},
+        {3, 2, 1}};
+
+    vector<vector<int>> C(N, vector<int>(N));
+
+    matrixMultiply(A, B, C);
+
+    assert(C[0] == vector<int>({30, 24, 18}));
+    assert(C[1] == vector<int>({84, 69, 54}));
+    assert(C[2] == vector<int>({138, 114, 90}));
+}
+
 void test()
 {
     testLoopDependence();
     testLoopIndependence();
+    testMatrixMultiply();
 }
 
 int main()
